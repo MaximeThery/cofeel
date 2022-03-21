790f30309bbf7d982916ed5f15d09775aef2944f
Lucas Dbr <lucas0000.milow@gmail.com>
2022-03-21 16:31:26 +0100

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,13 @@
-@extends('layout')
-
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login page</title>
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+</head>
+<body>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -70,4 +77,7 @@
         </div>
     </div>
 </div>
-@endsection
+</body>
+</html>
+
+
