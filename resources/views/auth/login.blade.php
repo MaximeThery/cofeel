cf907f79aa0ee1cc53575d50fc34c13ff7111581
Lucas Dbr <lucas0000.milow@gmail.com>
2022-03-22 20:49:58 +0100

    avancement login (mise en place du logo + formulaire)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,20 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login page</title>
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Fredoka One' rel='stylesheet'>
 </head>
 <body>
+    
 <div class="container">
+    <div class="div_logo_img"> <img src="{{ asset('cup.png') }}"class="logo_img"/></div>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <a href="/login">Conexion</a>
+            <a href="/register">Inscription</a>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -33,7 +37,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Mot de passe') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -60,13 +64,14 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                <button type="submit" class="btn-primary">
+                                   <span>{{ __('Login') }}</span>
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                <br>
+                                    <a class="btn btn-link" id="Forgot_Pasword" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password ?') }}
                                     </a>
                                 @endif
                             </div>
